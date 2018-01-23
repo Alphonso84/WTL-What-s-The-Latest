759275062cfcf97f13f74224da28175021bb336c
Alphonso Bret Sensley II <fonzman1984@me.com>
2018-01-22 19:24:42 -0800

    Initial Commit

---
+++ WTL What's The Latest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WTL What's The Latest
+//
+//  Created by user on 1/22/18.
+//  Copyright © 2018 Alphonso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
